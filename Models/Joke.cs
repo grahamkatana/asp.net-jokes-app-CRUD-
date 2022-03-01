e14ca0abb0347a9af28eacee4c46a2a0b92127fe
grahamkatana <graykatanakenny@gmail.com>
2022-03-01 12:10:48 +0200

    Initial commit

---
+++ Models/Joke.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyJokesApp.Models
+{
+    public class Joke
+    {
+        //prop shortcut
+        public int Id { get; set; }
+        public string JokeQuestion { get; set; }
+
+        public string JokeAnswer { get; set; }
+
+        //constructor
+
+        public Joke()
+        {
+
+        }
+    }
+}
